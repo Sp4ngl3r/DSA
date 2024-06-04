82b9e3ada1686a214949b1fcdec209604c8d2e06
Sp4ngl3r <hemanth921941@gmail.com>
2024-06-04 21:50:31 +0530

    Solved Leetcode - Two Sums Problem

---
+++ blind_75/TwoSum.java
@@ -0,0 +1,19 @@
+package blind_75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> differenceMap = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (differenceMap.containsKey(i)) {
+                return new int[]{differenceMap.get(i), i};
+            }
+            differenceMap.put(target - nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+}
