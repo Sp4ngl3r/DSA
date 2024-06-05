579c61e2b003dde09841560794fe91117ab3ac0d
Sp4ngl3r <hemanth921941@gmail.com>
2024-06-05 23:00:25 +0530

    Solved Leetcode - Maximum Product Subarray Problem

---
+++ blind_75/MaximumProductSubarray.java
@@ -0,0 +1,31 @@
+package blind_75;
+
+// https://leetcode.com/problems/maximum-product-subarray/description/
+public class MaximumProductSubarray {
+
+    public int maxProduct(int[] nums) {
+
+        int maximum = Integer.MIN_VALUE;
+        int currentMaximum = 1;
+        int currentMinimum = 1;
+
+        for (int num : nums) {
+
+            // If negative number is encountered, reverse the minimum and maximum.
+            if (num < 0) {
+                currentMaximum = currentMaximum + currentMinimum;
+                currentMinimum = currentMaximum - currentMinimum;
+                currentMaximum = currentMaximum - currentMinimum;
+            }
+
+            currentMinimum = Math.min(num, num * currentMinimum);
+            currentMaximum = Math.max(num, num * currentMaximum);
+
+            maximum = Math.max(currentMaximum, maximum);
+        }
+
+        return maximum;
+    }
+}
+
+
