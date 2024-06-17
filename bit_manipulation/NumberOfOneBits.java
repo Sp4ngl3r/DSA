18b646b6cdf8c2d9ce2f8d8ab1529937ab02b141
sp4ngl3r <hemanth921941@gmail.com>
2024-06-17 19:29:40 +0530

    Solved Leetcode - Number of 1 bits Problem

---
+++ bit_manipulation/NumberOfOneBits.java
@@ -0,0 +1,26 @@
+package bit_manipulation;
+
+///https://leetcode.com/problems/number-of-1-bits/description/
+public class NumberOfOneBits {
+
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n % 2;
+            n = n >> 1;
+        }
+
+        return count;
+    }
+
+    public int enhancedVersionOfHammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            ///This tries to remove the last set 1-bit in the binary number.
+            n = n & (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+}
---
+++ blind_75/array/ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+package blind_75.array;
+
+///https://leetcode.com/problems/container-with-most-water/description/
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int maxArea = 0, left = 0, right = height.length - 1;
+
+        ///Using Two Pointer Technique
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+}
