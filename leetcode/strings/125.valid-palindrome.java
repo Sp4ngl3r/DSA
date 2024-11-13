98091dd936e10f9ee74c82a3500ea373dec56445
Sp4ngl3r <hemanth921941@gmail.com>
2024-11-13 16:50:29 +0530

    problem: valid palindrome

---
+++ leetcode/strings/125.valid-palindrome.java
@@ -0,0 +1,47 @@
+package leetcode.strings;
+
+/*
+ * @lc app=leetcode id=125 lang=java
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        // Two Pointer approach
+        int left = 0;
+        int right = s.length() - 1;
+        s = s.toLowerCase();
+
+        while (left <= right) {
+
+            char currentLeftCharacter = s.charAt(left);
+            char currentRightCharacter = s.charAt(right);
+
+            // If the character is not letter or digit, ignore it
+            if (!Character.isLetterOrDigit(currentLeftCharacter)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(currentRightCharacter)) {
+                right--;
+            } else {
+
+                if (currentLeftCharacter != currentRightCharacter) {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+        }
+
+        return true;
+    }
+}
+// @lc code=end
