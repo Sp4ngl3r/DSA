4c3b009e86e14d692298311e8dfc81b1361ad092
Sp4ngl3r <hemanth921941@gmail.com>
2024-11-13 23:07:52 +0530

    problem: binary search

---
+++ leetcode/arrays/704.binary-search.java
@@ -0,0 +1,31 @@
+package leetcode.arrays;
+
+/*
+ * @lc app=leetcode id=704 lang=java
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+// @lc code=end
