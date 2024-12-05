8a42a542ee0954170d59523e4fcdd10b30189609
Sp4ngl3r <hemanth921941@gmail.com>
2024-12-05 16:42:52 +0530

    problem: flood fill

---
+++ leetcode/graphs/733.flood-fill.java
@@ -0,0 +1,56 @@
+package leetcode.graphs;
+
+/*
+ * @lc app=leetcode id=733 lang=java
+ *
+ * [733] Flood Fill
+ */
+
+// @lc code=start
+class Solution {
+    private void dfs(int[][] image, int row, int column, int currentColor, int newColor) {
+
+        int adjacentCells[][] = {
+                { 0, 1 }, // right
+                { 0, -1 }, // left
+                { 1, 0 }, // down
+                { -1, 0 } // up
+        };
+
+        // determine total number of rows and columns of the matrix
+        int m = image.length;
+        int n = image[0].length;
+
+        for (int[] cell : adjacentCells) {
+
+            // calculate neighbouring cell indices
+            int r = row + cell[0];
+            int c = column + cell[1];
+
+            // check if the cell is within the matrix
+            if (r >= 0 && r < m &&
+                    c >= 0 && c < n &&
+                    image[r][c] == currentColor) {
+                image[r][c] = newColor;
+                dfs(image, r, c, currentColor, newColor);
+            }
+
+        }
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+                    
+        if (image[sr][sc] == color) {
+            return image;
+        }
+
+        // set the starting cell to have new color
+        int currentColor = image[sr][sc];
+        image[sr][sc] = color;
+
+        dfs(image, sr, sc, currentColor, color);
+
+        return image;
+    }
+}
+// @lc code=end
