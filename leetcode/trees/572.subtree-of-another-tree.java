16588fb8bc6e5392fd4e5115506e7fe7f1948494
Sp4ngl3r <hemanth921941@gmail.com>
2025-01-21 10:39:48 +0530

    problem: subtree of another tree

---
+++ leetcode/trees/572.subtree-of-another-tree.java
@@ -0,0 +1,50 @@
+package trees;
+
+/*
+ * @lc app=leetcode id=572 lang=java
+ *
+ * [572] Subtree of Another Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+
+        if (isSame(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSame(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null || (p.val != q.val)) {
+            return false;
+        }
+
+        return isSame(p.left, q.left) && isSame(p.right, q.right);
+    }
+}
+// @lc code=end
