bbd5d875591bcfa664657791b97e2bfc1ca74e7c
Sp4ngl3r <hemanth921941@gmail.com>
2025-01-28 10:11:15 +0530

    problem: 3 sum

---
+++ leetcode/arrays/15.3-sum.java
@@ -0,0 +1,50 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=15 lang=java
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int low = i + 1, high = nums.length - 1;
+
+                while (low < high) {
+                    if (nums[low] + nums[high] + nums[i] == 0) {
+                        result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+
+                        while (low < high && nums[low] == nums[low + 1]) {
+                            low++;
+                        }
+
+                        while (low < high && nums[high] == nums[high - 1]) {
+                            high--;
+                        }
+
+                        low++;
+                        high--;
+                    } else if (nums[low] + nums[high] + nums[i] < 0) {
+                        low++;
+                    } else {
+                        high--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
+// @lc code=end
