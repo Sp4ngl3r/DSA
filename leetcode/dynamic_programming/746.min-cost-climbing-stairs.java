3d4327a65ef455293d443a7bf4473fa181dfbade
Sp4ngl3r <hemanth921941@gmail.com>
2026-03-17 14:23:38 +0530

    update: min cost climbing stairs

--- leetcode/dynamic_programming/746.min-cost-climbing-stairs.java
+++ leetcode/dynamic_programming/746.min-cost-climbing-stairs.java
@@ -8,17 +8,31 @@ package dynamic_programming;
 // @lc code=start
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int minCost[] = new int[n];
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
 
-        minCost[0] = cost[0];
-        minCost[1] = cost[1];
+        // int n = cost.length;
+        // int minCost[] = new int[n];
+        // minCost[0] = cost[0];
+        // minCost[1] = cost[1];
+        // for (int i = 2; i < n; i++) {
+        // minCost[i] = Math.min(minCost[i - 1], minCost[i - 2]) + cost[i];
+        // }
+        // return Math.min(minCost[n-1], minCost[n-2]);
+
+
+        // Space Complexity: O(1)
+        int n = cost.length;
+        int prev1 = cost[0];
+        int prev2 = cost[1];
 
         for (int i = 2; i < n; i++) {
-            minCost[i] = Math.min(minCost[i - 1], minCost[i - 2]) + cost[i];
+            int curr = Math.min(prev1, prev2) + cost[i];
+            prev1 = prev2;
+            prev2 = curr;
         }
 
-        return Math.min(minCost[n-1], minCost[n-2]);
+        return Math.min(prev1, prev2);
     }
 }
 // @lc code=end
