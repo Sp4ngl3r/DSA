3d8fab2061fa66583c11f162af34c75b1f505fff
Sp4ngl3r <hemanth921941@gmail.com>
2026-03-17 20:15:35 +0530

    problem: longest common subsequence

--- leetcode/dynamic_programming/1143.longest-common-subsequence.java
+++ leetcode/dynamic_programming/1143.longest-common-subsequence.java
@@ -9,7 +9,29 @@ package dynamic_programming;
 // @lc code=start
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        // TODO: Need to complete this problem
+        int m = text1.length();
+        int n = text2.length();
+        int dp[][] = new int[m + 1][n + 1];
+
+        // Populate first row and first column with zeroes
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 }
 // @lc code=end
