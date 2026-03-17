cf54333ad658cfc085be7160262325a79f69d036
Sp4ngl3r <hemanth921941@gmail.com>
2026-03-18 00:15:34 +0530

    problem: edit distance

--- leetcode/dynamic_programming/1143.longest-common-subsequence.java
+++ leetcode/dynamic_programming/1143.longest-common-subsequence.java
@@ -11,7 +11,7 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-        int dp[][] = new int[m + 1][n + 1];
+        int[][] dp = new int[m + 1][n + 1];
 
         // Populate first row and first column with zeroes
         for (int i = 0; i <= m; i++) {
@@ -23,6 +23,8 @@ class Solution {
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
+                // If the characters match, increment the diagonal value by 1;
+                // else compute the max value between upper and left cell values.
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
---
+++ leetcode/dynamic_programming/72.edit-distance.java
@@ -0,0 +1,36 @@
+package dynamic_programming;
+/*
+ * @lc app=leetcode id=72 lang=java
+ *
+ * [72] Edit Distance
+ */
+
+// @lc code=start
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // fill in first row and column with defaults
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
+// @lc code=end
